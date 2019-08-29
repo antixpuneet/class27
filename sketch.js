adddcc94b7223fc0b181779ba5598a9526631bab
antixpuneet <puneetmaser28573@gmail.com>
2019-08-29 16:53:52 +0530

    we add constraint useing class

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint; 
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg,platform,newlog;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -14,7 +15,6 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -33,8 +33,9 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    newlog = new Log(500,400,320,-PI/3);
     bird = new Bird(100,100);
+    chain = new Chain(bird.body,newlog.body);
 
 }
 
@@ -61,4 +62,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    newlog.display();
+    chain.display();
 }
